38800b32ea76acc633a3f4ff04e5db428696b3da
Yunyun548 <jordi.fernandez@y-nov.com>
2017-07-18 10:30:38 +0800

    Update Experiences logic

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var experiences = require('../public/data/work-experience.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
     res.render('index', {
-        title: 'Express',
+        experiences: experiences,
         location: 'Taipei, Taiwan'
     });
 });
