cd0dc759d09e2f8de70f3c3ead8d159cf6c3bb7d
Yunyun548 <jordi.fernandez@y-nov.com>
2017-07-18 14:28:06 +0800

    Skills Education and Links

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 var experiences = require('../public/data/work-experience.json');
+var education = require('../public/data/education.json');
+var skillsMains = require('../public/data/skills-mains.json');
+var skillsSecondaries = require('../public/data/skills-secondaries.json');
+var links = require('../public/data/links.json');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
     res.render('index', {
         experiences: experiences,
+        education: education,
+        skills: {
+            mains: skillsMains,
+            secondaries: skillsSecondaries
+        },
+        links: links,
         location: 'Taipei, Taiwan'
     });
 });
